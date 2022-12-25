e91b1c97b3d6fb158b37f8390b55e4703199471f
fathima.nafeesa <fathimanafeesavy@gmail.com>
2022-12-26 03:03:21 +0530

    updates

--- MovieCollection/MovieCollection/settings.py
+++ MovieCollection/MovieCollection/settings.py
@@ -87,66 +87,21 @@ WSGI_APPLICATION = 'MovieCollection.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mc_database',
         'USER': 'fathimanafeesa',
         'PASSWORD': 'aydin',
-        'PORT': '3306',
         'HOST': '127.0.0.1',
+        'PORT':'3306',
         'OS_THIRD_PARTY_URL': env('OS_THIRD_PARTY_URL'),
         'OS_THIRD_PARTY_AUTH': env('OS_THIRD_PARTY_AUTH'),
         'OS_THIRD_PARTY_PASSWORD': env('OS_THIRD_PARTY_PASSWORD')
 
-    }
+    },
 }
-
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/nth-rarity-372710:northamerica-northeast1:fathimanafeesa',
-            'USER': 'fathimanafeesa',
-            'PASSWORD': 'aydin',
-            'NAME': '[YOUR-DATABASE]',
-        }
-    }
-
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mc_database',
-        'USER': 'fathimanafeesa',
-        'PASSWORD': 'aydin',
-        'HOST': '/cloudsql/nth-rarity-372710:northamerica-northeast1:fathimanafeesa',
-        'OS_THIRD_PARTY_URL': env('OS_THIRD_PARTY_URL'),
-        'OS_THIRD_PARTY_AUTH': env('OS_THIRD_PARTY_AUTH'),
-        'OS_THIRD_PARTY_PASSWORD': env('OS_THIRD_PARTY_PASSWORD')
-
-    }
-    }
-else:
-    # Running locally so connect to either a local MySQL instance or connect
-    # to Cloud SQL via the proxy.  To start the proxy via command line:
-    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'NAME': 'mc_database',
-            'USER': 'fathimanafeesa',
-            'PASSWORD': 'aydin',
-            'OS_THIRD_PARTY_URL': env('OS_THIRD_PARTY_URL'),
-            'OS_THIRD_PARTY_AUTH': env('OS_THIRD_PARTY_AUTH'),
-            'OS_THIRD_PARTY_PASSWORD': env('OS_THIRD_PARTY_PASSWORD')
-
-        }
-    }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
